55a1a36c4cb44bfd2e8d8fc76303d1494c2324e6
1onehh <1425599319@11.com>
2025-05-11 10:42:16 +0800

    2

---
+++ src/part1.hpp
@@ -0,0 +1,87 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <stdexcept>
+#include <algorithm>
+
+using namespace std;
+
+class Matrix {
+public:
+    vector<vector<float>> data;
+    int rows, cols;
+    Matrix(int r, int c) : rows(r), cols(c) {
+        data = vector<vector<float>>(r, vector<float>(c, 0.0f));
+    }
+
+    Matrix(const vector<vector<float>>& d) {
+        data = d;
+        rows = d.size();
+        cols = d.empty() ? 0 : d[0].size();
+    }
+
+    static Matrix multiply(const Matrix& A, const Matrix& B) {
+        if (A.cols != B.rows)
+            throw std::invalid_argument("dimensions do not match");
+        Matrix result(A.rows, B.cols);
+        for (int i = 0; i < A.rows; ++i) {
+            for (int j = 0; j < B.cols; ++j) {
+                for (int k = 0; k < A.cols; ++k) {
+                    result.data[i][j] += A.data[i][k] * B.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    static Matrix add(const Matrix& A, const Matrix& B) {
+        if (A.rows != B.rows || A.cols != B.cols)
+            throw std::invalid_argument("dimensions do not match");
+        Matrix result(A.rows, A.cols);
+        for (int i = 0; i < A.rows; ++i) {
+            for (int j = 0; j < A.cols; ++j) {
+                result.data[i][j] = A.data[i][j] + B.data[i][j];
+            }
+        }
+        return result;
+    }
+};
+
+// ReLU 
+Matrix relu(const Matrix& input) {
+    Matrix result = input;
+    for (int i = 0; i < result.rows; ++i) {
+        for (int j = 0; j < result.cols; ++j) {
+            result.data[i][j] = max(0.0f, result.data[i][j]);
+        }
+    }
+    return result;
+}
+
+// SoftMax 
+vector<float> softmax(const vector<float>& input) {
+    vector<float> output(input.size());
+    float sum = 0.0f;
+    for (float val : input) {
+        sum += exp(val); 
+    }
+    for (int i = 0; i < input.size(); ++i) {
+        output[i] = exp(input[i]) / sum;
+    }
+    return output;
+}
+
+class Model {
+private:
+    Matrix weight1, bias1, weight2, bias2;//784 * 500 1 * 500 500 * 10 1 * 10
+public:
+    Model(const Matrix& w1, const Matrix& b1, const Matrix& w2, const Matrix& b2)
+        : weight1(w1), bias1(b1), weight2(w2), bias2(b2) {}
+    vector<float> forward(const Matrix& input) {
+        Matrix z1 = Matrix::add(Matrix::multiply(input, weight1), bias1);
+        Matrix a1 = relu(z1);
+        Matrix z2 = Matrix::add(Matrix::multiply(a1, weight2), bias2);
+        return softmax(z2.data[0]);
+    }
+};
---
+++ src/part2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "part1.hpp"
+#include "json.hpp"
+using json = nlohmann::json;
+using namespace std;
+// 从二进制文件加载矩阵
+Matrix loadMatrix(const string& filepath, int rows, int cols) {
+    Matrix mat(rows, cols);
+    ifstream file(filepath, ios::binary);
+    if (!file.is_open()) throw runtime_error("Failed to open ");
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            float value;
+            file.read((char*)(&value), sizeof(float));//二进制读取文件
+            if (!file) throw runtime_error("Failed to read data " + filepath);
+            mat.data[i][j] = value;
+        }
+    }
+    return mat;
+}
+
+// 加载模型
+Model loadModel(const string& path) {
+    ifstream metaFile(path + "/meta.json");
+    if (!metaFile.is_open()){
+        throw runtime_error("Cannot open meta.json");
+    }
+    json meta;
+    metaFile >> meta;
+    int w1r = meta["fc1.weight"][0];
+    int w1c = meta["fc1.weight"][1];
+
+    int b1r = meta["fc1.bias"][0];
+    int b1c = meta["fc1.bias"][1];
+
+    int w2r = meta["fc2.weight"][0];
+    int w2c = meta["fc2.weight"][1];
+
+    int b2r = meta["fc2.bias"][0];
+    int b2c = meta["fc2.bias"][1];
+
+    Matrix weight1 = loadMatrix(path + "/fc1.weight", w1r, w1c);
+    Matrix bias1   = loadMatrix(path + "/fc1.bias",   b1r, b1c);
+    Matrix weight2 = loadMatrix(path + "/fc2.weight", w2r, w2c);
+    Matrix bias2   = loadMatrix(path + "/fc2.bias",   b2r, b2c);
+
+    return Model(weight1, bias1, weight2, bias2);
+}
+
+int main() {
+    try {
+        Model model = loadModel("C:/Users/czx/Desktop/GKD-Software-2025-Test/mnist-fc");
+        cout << "Model loaded successfully." <<endl;
+    } catch (const exception& e) {
+        cerr << "Load failed: " << e.what() << endl;
+    }
+}
